d863b9b2923e9f6e69e6050bc07e29d330f0a2e9
anmol mahajan <anmolmahajan9@gmail.com>
2025-09-02 03:55:07 +0000

    getting this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 export const firebaseConfig = {
   projectId: "photo20-xx189",
   appId: "1:1097066143064:web:87f461f28faff93013192d",
-  storageBucket: "photo20-xx189.appspot.com",
+  storageBucket: "photo20-xx189.firebasestorage.app",
   apiKey: "AIzaSyAI-_WjiKfivl1LQAJEfjkjPK1Y9hVqEQE",
   authDomain: "photo20-xx189.firebaseapp.com",
   messagingSenderId: "1097066143064"
