9ccec8091e5b1531be1bf31a78d682f3593c3796
anmol mahajan <anmolmahajan9@gmail.com>
2025-09-06 05:57:45 +0000

    i have changed this piece of code because you left the API KEY out
    expo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
@@ -7,7 +8,7 @@ export const firebaseConfig = {
   projectId: "photo20-xx189",
   appId: "1:1097066143064:web:87f461f28faff93013192d",
   storageBucket: "photo20-xx189.firebasestorage.app",
-  apiKey: GEMINI_API_KEY,
+  apiKey: "REDACTED",
   authDomain: "photo20-xx189.firebaseapp.com",
   messagingSenderId: "1097066143064"
 };
