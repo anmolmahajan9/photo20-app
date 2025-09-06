be78c07923f025394d590ac774b455cbd8731b09
anmol mahajan <anmolmahajan9@gmail.com>
2025-09-06 01:11:29 +0000

    failed to signin with google. tell me which files are resp for this whos

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getFirestore } from 'firebase/firestore';
 export const firebaseConfig = {
   projectId: "photo20-xx189",
   appId: "1:1097066143064:web:87f461f28faff93013192d",
-  storageBucket: "photo20-xx189.firebasestorage.app",
+  storageBucket: "photo20-xx189.appspot.com",
   apiKey: "AIzaSyB-m4y1_3a6-n7K_q4N5f6g7h8i9j0k1l2",
   authDomain: "photo20-xx189.firebaseapp.com",
   messagingSenderId: "1097066143064"
