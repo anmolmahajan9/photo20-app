0442a0959e1cf17969be8e66ac9baf9be0e63bef
anmol mahajan <anmolmahajan9@gmail.com>
2025-09-06 05:58:33 +0000

    where can i find the key?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
@@ -8,7 +7,7 @@ export const firebaseConfig = {
   projectId: "photo20-xx189",
   appId: "1:1097066143064:web:87f461f28faff93013192d",
   storageBucket: "photo20-xx189.firebasestorage.app",
-  apiKey: "REDACTED",
+  apiKey: "AIzaSyB-m4y1_3a6-n7K_q4N5f6g7h8i9j0k1l2",
   authDomain: "photo20-xx189.firebaseapp.com",
   messagingSenderId: "1097066143064"
 };
